1d52f6d8ca52c2c5abc61c517d76d09b228a0f59
Hemanthkumar Chereddy <hemanthkumarchereddy@HemanthrsLaptop.attlocal.net>
2024-08-21 19:49:58 -0500

    Top bar - Search input field added

--- react-admin/src/scenes/global/TopBar.jsx
+++ react-admin/src/scenes/global/TopBar.jsx
@@ -1,5 +1,36 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsModeOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/PersonOutline";
+import SearchModeOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const TopBar = () => {
-  return <div>TopBar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARVCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      <IconButton></IconButton>
+    </Box>
+  );
 };
 
 export default TopBar;
