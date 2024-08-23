60f56a1f34cae9c26820fc5d28823bb421db4195
Hemanthkumar Chereddy <hemanthkumarchereddy@HemanthrsLaptop.attlocal.net>
2024-08-22 20:51:38 -0500

    Invoices : Data grid added

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -11,6 +11,8 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Forms from "./scenes/forms";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
@@ -66,9 +68,9 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/*  <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Forms />} />
+              {/*  <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
