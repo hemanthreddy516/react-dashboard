f24006a6b92a72bb57df2839a02172db9c995e9f
Hemanthkumar Chereddy <hemanthkumarchereddy@HemanthrsLaptop.attlocal.net>
2024-08-23 00:36:27 -0500

    Dashboard : 1st row added

--- react-admin/src/scenes/dashboard/index.jsx
+++ react-admin/src/scenes/dashboard/index.jsx
@@ -1,7 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Button, useTheme } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import Geography from "../geography";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <Box m={"20px"}>
       <Box
@@ -13,6 +28,118 @@ const Dashboard = () => {
           title={"DASHBOARD"}
           subTitle={"Welcome to your dashboard"}
         ></Header>
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            DOWNLAOD REPORTS
+          </Button>
+        </Box>
+      </Box>
+      {/* GRID & CHARTS */}
+      <Box
+        display={"grid"}
+        gridTemplateColumns={"repeat(12,1fr)"}
+        gridAutoRows={"140px"}
+        gap={"20px"}
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn={"span 3"}
+          backgroundColor={colors.primary[400]}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <StatBox
+            title={"12,321"}
+            subTitle={"Email sent"}
+            progress={"0.75"}
+            increase={"+14%"}
+            icon={
+              <EmailIcon
+                sx={{
+                  color: colors.greenAccent[600],
+                  fontSize: "26px",
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn={"span 3"}
+          backgroundColor={colors.primary[400]}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <StatBox
+            title={"12,321"}
+            subTitle={"Sales Obtained"}
+            progress={"0.5"}
+            increase={"+21%"}
+            icon={
+              <PointOfSaleIcon
+                sx={{
+                  color: colors.greenAccent[600],
+                  fontSize: "26px",
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn={"span 3"}
+          backgroundColor={colors.primary[400]}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <StatBox
+            title={"163,321"}
+            subTitle={"New Clients"}
+            progress={"0.35"}
+            increase={"+5%"}
+            icon={
+              <PersonAddIcon
+                sx={{
+                  color: colors.greenAccent[600],
+                  fontSize: "26px",
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn={"span 3"}
+          backgroundColor={colors.primary[400]}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <StatBox
+            title={"22,321"}
+            subTitle={"Traffic Inbound"}
+            progress={"0.80"}
+            increase={"+43%"}
+            icon={
+              <TrafficIcon
+                sx={{
+                  color: colors.greenAccent[600],
+                  fontSize: "26px",
+                }}
+              />
+            }
+          />
+        </Box>
       </Box>
     </Box>
   );
