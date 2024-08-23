8f3b10c30919e66230a7466abd4c9549672b7f83
Hemanthkumar Chereddy <hemanthkumarchereddy@HemanthrsLaptop.attlocal.net>
2024-08-22 21:41:52 -0500

    Calendar page : Calendar page added

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -13,6 +13,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Forms from "./scenes/forms";
+import Calendar from "./scenes/calendar";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
@@ -70,11 +71,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Forms />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/*  <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+               */}
             </Routes>
           </main>
         </div>
